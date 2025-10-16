bb8ed5a2b9c26863e35a2098d8e7fee5059217ed
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-16 13:25:17 -0700

    Basic increase function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import React from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
+  function increaseClick() {
+    setCount(count + 1);
+  }
   return (
     <>
-
+      <img onClick={increaseClick}></img>
     </>
   )
 }
